4273f76c8b16f5530dcf94f64dbad6527aad502f
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2020-07-11 10:23:42 -0700

    Add files via upload

---
+++ userdemo/frontend/build/precache-manifest.87dfeafd175da598ca7890a184bcde0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5b6ab228b9ea6af587283c047add3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d8db51e02716921e8b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf6001f578082f484598",
+    "url": "/static/js/2.a7c919e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a7c919e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d8db51e02716921e8b",
+    "url": "/static/js/main.b563299c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
